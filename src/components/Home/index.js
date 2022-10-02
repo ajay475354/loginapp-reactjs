bbbf3f9162f6a9eec827b3bc5609945aa623c7f1
ajay475354 <arepalliajaykumar@gmail.com>
2022-10-02 08:12:37 +0000

    app login

---
+++ src/components/Home/index.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import './index.css'
+import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
+class Home extends Component {
+  state = {isLogin: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLogin: !prevState.isLogin}))
+  }
+
+  render() {
+    const {isLogin} = this.state
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <Message login={isLogin} />
+          {isLogin ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/Message/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Message = props => {
+  const {login} = props
+  const message = login ? 'Welcome User' : 'Please Login'
+  return <h1 className="message">{message}</h1>
+}
+
+export default Message
